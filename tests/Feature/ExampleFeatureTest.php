96129e735830d5eb419aeb6925201b4279f661dd
Abdurozzaq Nurul Hadi <example@gmail.com>
2025-08-28 13:50:41 +0700

    Fix

---
+++ tests/Feature/ExampleFeatureTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class ExampleFeatureTest extends TestCase
+{
+    /** @test */
+    public function it_runs_a_basic_feature_check()
+    {
+        $this->assertTrue(true);
+    }
+}
